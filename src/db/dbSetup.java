b16e3bb633b7c6b703597a05c34ae147a83b7651
Christopher Tran <ctran457@gmail.com>
2021-03-07 10:04:36 -0600

    Added dbSetup.java

---
+++ src/db/dbSetup.java
@@ -0,0 +1,11 @@
+package db;
+
+/*
+CSCE 315
+Database setup example
+Purpose: Keeping usernames and password private.
+*/
+public final class dbSetup {
+  public static final String user = "tchristopher457";
+  public static final String pswd = "627002599";
+}//end class
