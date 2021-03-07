f9f4b4346f2d8dcf7bfc040e9366579b9b87bcfb
Bora Oztekin <borao@users.noreply.github.com>
2021-03-07 00:24:48 -0600

    WIP Implement order class

--- src/api/Order.java
+++ src/api/Order.java
@@ -3,7 +3,32 @@ package api;
 import java.util.ArrayList;
 
 public class Order {
-    Order(ArrayList<Item> items) {
+    int id;
+    Customer customer;
+    Employee employee;
+    String date;
+    String time;
+    String contents;
+    boolean fulfilled;
 
+    public Order(ArrayList<Item> items, Customer customer, Employee employee, boolean fulfilled) {
+        String contents = makeItemListString(items);
+        this.customer = customer;
+        this.employee = employee;
+        this.date = java.time.LocalDate.now().toString();
+        this.time = java.time.LocalTime.now().toString();
+        this.contents = contents;
+        this.fulfilled = fulfilled;
+        // Insert into database
+        this.id = id;
+    }
+
+    static String makeItemListString(ArrayList<Item> items) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Item item : items) {
+            String typeCode = item.getClass().getName().substring(0, 1);
+            stringBuilder.append(typeCode).append(item.id).append(" ");
+        }
+        return stringBuilder.toString();
     }
 }
