f37cc8509ca5f0237f4d68c1b93ced243ec4db25
Christopher Tran <ctran457@gmail.com>
2021-03-07 13:40:45 -0600

    refactored part of Order.java

--- src/api/Order.java
+++ src/api/Order.java
@@ -9,6 +9,13 @@ import java.util.HashMap;
 
 public class Order {
     static final String tableName = "orders";
+    static final String id_column = "id";
+    static final String customer_id_column = "customer_id";
+    static final String employee_id_column = "employee_id";
+    static final String date_column = "date";
+    static final String time_column = "time";
+    static final String contents_column = "contents";
+    static final String fulfilled_column = "fulfilled";
 
     int id;
     Customer customer;
@@ -29,15 +36,19 @@ public class Order {
         // Insert into database
         HashMap<String, String> values = new HashMap<>();
         if (this.customer != null) {
-            values.put("customer_id", this.customer.id.toString());
+            values.put(Order.customer_id_column, this.customer.id.toString());
+        } else {
+            values.put(Order.customer_id_column, null);
         }
         if (this.employee != null) {
-            values.put("employee_id", this.employee.id.toString());
+            values.put(Order.employee_id_column, this.employee.id.toString());
+        } else {
+            values.put(Order.employee_id_column, null);
         }
-        values.put("date", this.date);
-        values.put("time", this.time);
-        values.put("contents", this.contents);
-        values.put("fulfilled", this.fulfilled ? "true" : "false");
+        values.put(Order.date_column, this.date);
+        values.put(Order.time_column, this.time);
+        values.put(Order.contents_column, this.contents);
+        values.put(Order.fulfilled_column, this.fulfilled ? "true" : "false");
 
         int updateResult = QueryBuilder.executeUpdate(QueryBuilder.buildInsertionQuery(Order.tableName, values));
 
@@ -48,7 +59,7 @@ public class Order {
         }
 
         if (orderResult != null) {
-            this.id = Integer.parseInt(orderResult.get(0).get("id"));
+            this.id = Integer.parseInt(orderResult.get(0).get(Order.id_column));
         }
     }
 
@@ -63,15 +74,19 @@ public class Order {
         // Insert into database
         HashMap<String, String> values = new HashMap<>();
         if (this.customer != null) {
-            values.put("customer_id", this.customer.id.toString());
+            values.put(Order.customer_id_column, this.customer.id.toString());
+        } else {
+            values.put(Order.customer_id_column, null);
         }
         if (this.employee != null) {
-            values.put("employee_id", this.employee.id.toString());
+            values.put(Order.employee_id_column, this.employee.id.toString());
+        } else {
+            values.put(Order.employee_id_column, null);
         }
-        values.put("date", this.date);
-        values.put("time", this.time);
-        values.put("contents", this.contents);
-        values.put("fulfilled", this.fulfilled ? "true" : "false");
+        values.put(Order.date_column, this.date);
+        values.put(Order.time_column, this.time);
+        values.put(Order.contents_column, this.contents);
+        values.put(Order.fulfilled_column, this.fulfilled ? "true" : "false");
 
         int updateResult = QueryBuilder.executeUpdate(QueryBuilder.buildInsertionQuery(Order.tableName, values));
 
@@ -82,7 +97,7 @@ public class Order {
         }
 
         if (orderResult != null) {
-            this.id = Integer.parseInt(orderResult.get(0).get("id"));
+            this.id = Integer.parseInt(orderResult.get(0).get(Order.id_column));
         }
     }
 
